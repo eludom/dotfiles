23a20c0aee7da0eb7eeb48ae0cf92cdd9f6a51d7
George Jones <gmj@pobox.com>
2013-05-01 06:53:07 -0400

    added binding for man pages, removed  ~/.emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,10 +36,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(display-time-mode t))
+ '(display-time-mode t)
+ '(org-ditaa-jar-path "/Users/gmj/elisp/old/org-mode/contrib/scripts/ditaa.jar"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 240 :width normal :foundry "apple" :family "Monaco")))))
+(put 'downcase-region 'disabled nil)
