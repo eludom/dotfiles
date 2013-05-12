14163366f2f7c888f82ba7440e2cb215c4154e2d
George Jones <gmj@pobox.com>
2013-05-12 08:55:38 +0000

    Generalized installer

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -4,19 +4,43 @@
 #
 # run this in dotfiles/ after git clone of git@github.com:eludom/dotfiles.git
 
-export DOTFILES=$PWD
+DOTFILES=$PWD
+NOW=`date "+%s"`
 
-cd $HOME
+#if [ -f .bashrc ]; then
+#  mv .bashrc .bashrc.$$.old
+#fi
+#ln -s ${DOTFILES}/.bashrc .
 
-if [ -f .bashrc ]; then
-  mv .bashrc .bashrc.$$.old
-fi
-ln -s ${DOTFILES}/.bashrc .
+#ln -s ${DOTFILES}/.bashrc .  
 
-ln -s ${DOTFILES}/.bashrc .  
-ln -s ${DOTFILES}/bin .
-ln -s ${DOTFILES}/lisp .
-ln -s ${DOTFILES}/emacs.d .
+#
+# Link directories and files in directories to here
+#
 
+for dirName in [ bin lisp .emacs.d ]; do
 
+    if [ -L ${HOME}/${dirName} ]; then
+	# we already linked this.  Do nothing
+	:
+    elif [ -d ${HOME}/${dirName} ]; then
+	: # this is a directory.  Link files that do not exist.
 
+	cd ${dirName}
+
+	for x in  * .[a-zA-Z]*; do
+	    if [ ! -f ${HOME}/${dirName} ]; then
+		ln -s $PWD/$x ${HOME}/${dirName}/$x
+	    fi
+	done
+
+	cd ..
+    else
+	ln -s $PWD/$dirName $HOME
+    fi
+
+done
+
+#
+# Link individal files to to here
+#
