148b6792c75b4ec0b2c4040d8c4136712c1f274f
George Jones <gmj@pobox.com>
2013-11-30 12:44:43 +0000

    Standardize org mode loading order

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,20 @@
 ; $ git clone git://orgmode.org/org-mode.git
 ; $ make autoloads
 
+; standard eamcs.d stuff first
 (add-to-list 'load-path "~/.emacs.d")
+
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
+;; embedded in literate Org-mode files.
+
+;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+; Set up basic org-mode stuff here because the rest of this init file
+; is going to use org
+
 (add-to-list 'load-path "~/src/org-mode/lisp")
 (add-to-list 'load-path "~/src/org-mode/contrib/lisp" t)
 
@@ -25,32 +38,6 @@
         (lambda ()
 	      (setq Info-additional-directory-list Info-default-directory-list)
 	      ))
-
-
-(add-to-list 'load-path "~/src/g-client")
-
-;;; init.el --- Where all the magic begins
-;;
-;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
-;; embedded in literate Org-mode files.
-
-;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
-(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-
-;(let* ((org-dir (expand-file-name
-;                 "lisp" (expand-file-name
-;                         "org" (expand-file-name
-;                                "src" dotfiles-dir))))
-;       (org-contrib-dir (expand-file-name
-;                         "lisp" (expand-file-name
-;                                 "contrib" (expand-file-name
-;                                            ".." org-dir))))
-;       (load-path (append (list org-dir org-contrib-dir)
-;                          (or load-path nil))))
-;  ;; load up Org-mode and Org-babel
-;  (require 'org-install)
-;  (require 'ob-tangle))
-
 (require 'org)
 (require 'org-install)
 (require 'ob-tangle)
