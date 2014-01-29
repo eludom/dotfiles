618178607c748d388bc8948f12903f864d41ed78
George Jones <gmj@pobox.com>
2014-01-29 05:49:52 -0500

    Added ox-bibtex example from Eric Shulte

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,7 +10,7 @@
 ; $ make autoloads
 
 ; standard eamcs.d stuff first
-(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/orgified-init")
 
 ;;; init.el --- Where all the magic begins
 ;;
@@ -18,7 +18,8 @@
 ;; embedded in literate Org-mode files.
 
 ;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
-(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+;(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+(setq dotfiles-dir "~/.emacs.d/orgified-init")
 
 ; Set up basic org-mode stuff here because the rest of this init file
 ; is going to use org
@@ -41,6 +42,7 @@
 (require 'org)
 (require 'org-install)
 (require 'ob-tangle)
+(require 'ox-bibtex)
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
@@ -58,4 +60,4 @@
 
 ; load specific files
 
-(load "bibhook.el")
+;(load "bibhook.el")
