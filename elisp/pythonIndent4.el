91f28b0f85fca9efb4702fe6ec310a2e85bf58c6
George Jones <gmj@pobox.com>
2015-05-09 06:31:05 -0400

    Major cleanup to reflect reality.

--- elisp/pythonIndent4.el
+++
@@ -1,8 +0,0 @@
-;; http://stackoverflow.com/questions/3684738/how-do-i-set-emacs-tab-size-to-4-chars-wide-for-py-files
-
-(add-hook 'python-mode-hook
-      (lambda ()
-        (setq indent-tabs-mode t)
-        (setq tab-width 4)
-        (setq python-indent 4)))
-
