df2278d059120a4c08d04788faa16a8a097c7241
George Jones <gmj@pobox.com>
2015-06-05 03:32:03 -0700

    chmod

--- install.sh
+++ install.sh
@@ -1,94 +1,23 @@
 #! /bin/bash
 #
-# Usage:
-#  installDotFiles [install|delete|list]
-#
-# Install my dotfiles on a new system. 
-#
-# Symlink everything from ~/ to ~/git/github.com/eludom/dotfiles
+# Create a symlink in ~ to the current directory
 #
 
-set -u -e
+set -e -u
 
-if [ $# -eq 0 ]; then
-    op="install"
-elif [ $# -eq 1 ]; then
-    op=$1
-else
-    echo too many aruments
-    echo usage: $0 [install|delete|list]
-    exit
-fi
+REALDIR=`pwd`
+mkdir -p ${REALDIR}
+cd ${REALDIR}
 
-GITREPO="git://github.com/eludom/dotfiles.git"
-GITHUBDIR="${HOME}/git/github.com/eludom"
-DOTFILES=${GITHUBDIR}/dotfiles
+now=`date "+%Y%m%d:%H%M"`
 
-NOW=`date "+%Y-%m-%d:%H:%M:%S"`
+linkThese=( .bashrc .gitconfig bin)
 
-if [ ! -d ${DOTFILES} ]; then
-    echo "installing dotfiles from github"
+for linkThis in ${linkThese[@]}; do
 
-    if [ -d GITHUBDIR ]; then
-	cd ${GITHUBDIR}
-	git pull ${GITREPO}
-    else
-	mkdir -p ${GITHUBDIR}
-	cd ${GITHUBDIR}
-	git clone ${GITREPO}
+    if [ -e ~/${linkThis} ]; then
+	echo mv ~/${linkThis} ~/${linkThis}.${now}
     fi
-fi
 
-
-# install
-
-#linkThese=( .bashrc .gitconfig bin)
-#linkThese=( .bashrc bin)
-cd ${HOME}
-
-
-if [ "$op" == "install" ]; then
-
-  for linkThis in ${linkThese[@]}; do
-    if [ -L ${HOME}/${linkThis} ]; then
-      rm ${HOME}/${linkThis} # always redo the link in case I move things
-      ln -s ${DOTFILES}/${linkThis} .
-    elif [ -f ${HOME}/${linkThis} ]; then
-      mv ${linkThis} ${linkThis}.${NOW}.old
-      ln -s ${DOTFILES}/${linkThis} .
-    elif [ -d ${HOME}/${linkThis} ]; then
-       mv ${linkThis} ${linkThis}.${NOW}.old
-       ln -s ${DOTFILES}/${linkThis} .
-    else
-      ln -s ${DOTFILES}/${linkThis} .
-    fi
-  done
-
-# delete
-
-elif [ "$op" == "delete" ]; then
-  for linkThis in ${linkThese[@]}; do
-    if [ -L ${HOME}/${linkThis} ]; then
-	rm ${HOME}/${linkThis}
-    fi
-  done
-
-# list
-
-elif [ "$op" == "list" ]; then
-  for linkThis in ${linkThese[@]}; do
-    if [ -e ${HOME}/${linkThis} ]; then
-	file ${HOME}/${linkThis}
-    else
-	echo ${HOME}/${linkThis} does not exist
-    fi
-  done
-
-else
-  echo bad option $op. Should be one of insall or delete
-fi
-
-
-
-
-
+    echo ln -s ~/${linkThis} ${linkThis}
+done
