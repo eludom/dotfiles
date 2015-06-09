a82751109766ac0f57980106be300b8a142c2d25
George Jones <gmj@pobox.com>
2015-06-09 05:06:29 -0700

    Use link2

--- install.sh
+++ install.sh
@@ -1,23 +1,13 @@
 #! /bin/bash
 #
-# Create a symlinks in ~ to a a list of things in the current directory. Save old versions.
+# Link files from this directory into $HOME
 #
 
 set -e -u
 
-REALDIR=`pwd`
-mkdir -p ${REALDIR}
-cd ${REALDIR}
-
-now=`date "+%Y%m%d:%H%M"`
-
+linkTo=$HOME
 linkThese=( .bashrc .gitconfig bin)
 
 for linkThis in ${linkThese[@]}; do
-
-    if [ -e ~/${linkThis} ]; then
-	mv ~/${linkThis} ~/${linkThis}.${now}
-    fi
-
-    ln -s ${REALDIR}/${linkThis} ~/${linkThis} 
+  link2 $linkThis $linkTo
 done
