ff8b33059813c3f46c9775bcee44603edc60cd67
George Jones <gmj@pobox.com>
2016-02-13 12:07:32 +0000

    Reorg dotfiles, ~/bin and installers

---
+++ install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+# Symlink specific files from this directory in to ~/
+
+set -e -u
+
+# Get the path to this (install.sh) script.
+#   See http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+linkTo=$HOME
+linkThese=( .bashrc .gitconfig .signature)
+link2=~/bin/link2
+
+for linkThis in ${linkThese[@]}; do
+  ${link2} -r ${DIR}/$linkThis $linkTo
+done
